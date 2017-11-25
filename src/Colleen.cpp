bae4cf909762b3b8fa8f67f4c719ef9d554a0437
qhonore <qhonore@student.42.fr>
2017-11-25 16:13:59 +0100

    Colleen.cpp OK

--- src/Colleen.cpp
+++ src/Colleen.cpp
@@ -1,2 +1,38 @@
 #include <iostream>
-int main (){std::string i="#include <iostream>";std::cout<<i;}
+#include <string>
+/*
+	Comment
+*/
+void func(void){}
+int main (){char q = 34;std::string c[]={
+"#include <iostream>",
+"#include <string>",
+"/*",
+"	Comment",
+"*/",
+"void func(void){}",
+"int main (){char q = 34;std::string c[]={",
+"};",
+"/*",
+"	Comment2",
+"*/",
+"func();",
+"for (int i = 0; i < 7; i++)",
+"	std::cout << c[i] << std::endl;",
+"for (int i = 0; i < 19; i++)",
+"	std::cout << q << c[i] << q << ',' << std::endl;",
+"for (int i = 7; i < 19; i++)",
+"	std::cout << c[i] << std::endl;",
+"}",
+};
+/*
+	Comment2
+*/
+func();
+for (int i = 0; i < 7; i++)
+	std::cout << c[i] << std::endl;
+for (int i = 0; i < 19; i++)
+	std::cout << q << c[i] << q << ',' << std::endl;
+for (int i = 7; i < 19; i++)
+	std::cout << c[i] << std::endl;
+}
